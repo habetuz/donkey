dd2789fabc5fd0b7eb73aa28a84ba3c1eee24ff8
Marvin Fuchs <marfinn3@gmail.com>
2025-04-08 10:56:08 +0000

    feat!: Add donkey images

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,63 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const images = ref<string[]>([])
+
+const loadImages = () => {
+  const context = import.meta.glob('/public/images/*')
+  for (const path in context) {
+    images.value.push(path.replace('/public', ''))
+  }
+}
+
+loadImages()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <h1>Discover the Beauty of Donkeys</h1>
+  <p>
+    Welcome to our website, where we celebrate the charm and elegance of donkeys through stunning
+    imagery.
+  </p>
+  <div class="image-grid">
+    <div v-for="src in images" class="image-item">
+      <img :src="src" alt="Image" />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+h1 {
   text-align: center;
-  margin-top: 2rem;
+  font-size: 2.5rem;
+  margin-top: 20px;
+  color: #333;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+p {
+  text-align: center;
+  font-size: 1.2rem;
+  margin: 10px 0 20px;
+  color: #555;
+}
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  width: 80%;
+  margin: 0 auto;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.image-item img {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 3 / 4;
+  object-fit: cover;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.image-item img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
 }
 </style>
